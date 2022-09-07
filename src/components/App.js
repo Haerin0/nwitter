c1aabe4eee7d3d1af9661c8f1c08609c1554f58e
Haerin0 <parkhaerin@gmail.com>
2022-09-07 09:27:09 +0900

    firebase.js edited

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import AppRouter from "./Router";
+import { authService } from "../firebase";
 
 function App() {
   const [isLoggedIn, setisLoggedIn] = useState(false);
+  console.log(authService.currentUser); //로그인한 상태가 아니면 null을 반환
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn} />
-    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
 }

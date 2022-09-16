7514d27a92c342ace50e0a57c2aaaee47800e14e
Haerin0 <parkhaerin@gmail.com>
2022-09-16 14:58:39 +0900

    set id for each data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,15 +5,21 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
 
     const getNweets = async () => {
         const dbNweets = await getDocs(collection(dbService, "nweets"));
-        dbNweets.forEach((document) => console.log(document.data()));
+        dbNweets.forEach((document) => {
+            const nweetObject = {...document.data(), id: document.id};
+            setNweets((prev) => [nweetObject, ...prev])
+        });
     };
 
     useEffect(() => {
         getNweets();
     },[]);
+
+    console.log(nweets);
 
     const onSubmit = async (e) => {
         e.preventDefault();

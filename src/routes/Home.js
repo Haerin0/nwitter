39950fbfc94b3bf83545ce533185b490b4f6c9b4
Haerin0 <parkhaerin@gmail.com>
2022-09-19 14:50:15 +0900

    save files in db

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,9 @@ import { dbService } from "fbase";
 import { addDoc, collection, getDocs, query,orderBy,onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import Nweet from "../components/Nweet";
+import { storageService } from "../fbase";
+import {v4 as uuuidv4} from "uuid"
+import { ref, uploadString } from "@firebase/storage";
 
 
 const Home = ({ userObj }) => {
@@ -25,21 +28,25 @@ const Home = ({ userObj }) => {
     
     const onSubmit = async (event) => {
         event.preventDefault();
-    try { const docRef = await addDoc(collection(dbService, "nweets"), {
-        text: nweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-    });
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-        setNweet("");
-    };
+
+        const fileRef = ref(storageService, `${userObj.uid}/${uuuidv4()}`);
+        const response = await uploadString(fileRef, attachment, "data_url");
+        console.log(response);
+        // try { const docRef = await addDoc(collection(dbService, "nweets"), {
+        //     text: nweet,
+        //     createdAt: Date.now(),
+        //     creatorId: userObj.uid,
+        // });
+        // } catch (e) {
+        //     console.error("Error adding document: ", e);
+        // }
+        //     setNweet("");
+         };
     const onChange = (event) => {
-    const {
-        target: { value },
-    } = event;
-        setNweet(value);
+        const {
+            target: { value },
+        } = event;
+            setNweet(value);
     };
 
     const onFileChange = (event) => {
